cc247d2b2a6db16d66c9ec0bbd71887dc1097a8b
Katherine Jones <katherine.jones@btinternet.com>
2020-02-02 11:14:27 +0000

    Game creation

---
+++ TennisMatch.Tests/GameCreation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using TennisMatch.Models;
+
+namespace TennisMatch.Tests
+{
+    [TestFixture]
+    public class GameCreation
+    {
+        [Test]
+        public void WhenNeitherPlayerHasWonTheGame_ThenAnErrorIsThrown()
+        {
+            // Not trying to handle mid-game scores so expect a game to have been completed
+            var ex = Assert.Throws<Exception>(() => new Game(ScoreValues.Forty, ScoreValues.Thirty));
+            ex.Message.Should().Be("Incomplete game - expect a player to have won the game");
+        }
+
+        [Test]
+        public void WhenBothPlayersHaveAWinningScore_ThenAnErrorIsThrown()
+        {
+            var ex = Assert.Throws<Exception>(() => new Game(ScoreValues.GameWon, ScoreValues.GameWon));
+            ex.Message.Should().Be("It is impossible for both players to win the game");
+        }
+
+        [Test]
+        [TestCase(ScoreValues.Love)]
+        [TestCase(ScoreValues.Fifteen)]
+        [TestCase(ScoreValues.Thirty)]
+        [TestCase(ScoreValues.Forty)]
+        public void WhenPlayer1Wins_ThenTheGameIsCreated(ScoreValues player2Score)
+        {
+            var game = new Game(ScoreValues.GameWon, player2Score);
+
+            game.Player1Score.Should().Be(ScoreValues.GameWon);
+            game.Player2Score.Should().Be(player2Score);
+        }
+
+        [Test]
+        [TestCase(ScoreValues.Love)]
+        [TestCase(ScoreValues.Fifteen)]
+        [TestCase(ScoreValues.Thirty)]
+        [TestCase(ScoreValues.Forty)]
+        public void WhenPlayer2Wins_ThenTheGameIsCreated(ScoreValues player1Score)
+        {
+            var game = new Game(player1Score, ScoreValues.GameWon);
+
+            game.Player1Score.Should().Be(player1Score);
+            game.Player2Score.Should().Be(ScoreValues.GameWon);
+        }
+    }
+}
--- TennisMatch/Models/Game.cs
+++ TennisMatch/Models/Game.cs
@@ -1,10 +1,21 @@
-﻿namespace TennisMatch.Models
+﻿using System;
+
+namespace TennisMatch.Models
 {
     public class Game
     {
         public ScoreValues Player1Score { get; }
         public ScoreValues Player2Score { get; }
 
-        public Game(ScoreValues player1Score, ScoreValues player2Score) => (Player1Score, Player2Score) = (player1Score, player2Score);
+        public Game(ScoreValues player1Score, ScoreValues player2Score)
+        {
+            if (player1Score == ScoreValues.GameWon && player2Score == ScoreValues.GameWon)
+                throw new Exception("It is impossible for both players to win the game");
+            if (player1Score != ScoreValues.GameWon && player2Score != ScoreValues.GameWon)
+                throw new Exception("Incomplete game - expect a player to have won the game");
+
+            Player1Score = player1Score;
+            Player2Score = player2Score;
+        }
     }
 }
