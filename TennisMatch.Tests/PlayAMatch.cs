d580e034812c2efd492c09c28e42960fefd76475
Katherine Jones <katherine.jones@btinternet.com>
2020-02-02 12:59:25 +0000

    Play a match

--- TennisMatch.Tests/PlayAMatch.cs
+++ TennisMatch.Tests/PlayAMatch.cs
@@ -1,6 +1,10 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Moq;
 using NUnit.Framework;
 using TennisMatch.Models;
+using Match = TennisMatch.Models.Match;
 
 namespace TennisMatch.Tests
 {
@@ -13,11 +17,101 @@ namespace TennisMatch.Tests
             var player1 = new Player("Fred Perry");
             var player2 = new Player("Andy Murray");
 
-            var match = new Match(player1, player2);
+            var gameResultGenerator = new GameResultGenerator(new RandomGenerator());
+
+            var match = new Match(player1, player2, gameResultGenerator);
 
             match.Player1.Should().BeEquivalentTo(player1);
             match.Player2.Should().BeEquivalentTo(player2);
             match.Sets.Should().BeEmpty();
+            match.Winner.Should().BeNull();
+        }
+
+        [Test]
+        public void WhenPlayer1WinsTheFirstTwoSets_ThenTheyWinTheMatch()
+        {
+            var player1 = new Player("Roger Federer");
+            var player2 = new Player("Andy Murray");
+
+            var gameResultGenerator = GetGeneratorForSetsWon(new[] { true, true });
+
+            var match = new Match(player1, player2, gameResultGenerator);
+
+            match.Play();
+
+            match.Winner.Should().Be(player1);
+        }
+
+        [Test]
+        public void WhenTheFirstTwoSetsAreSharedAndPlayer1WinsTheThird_ThenPlayer1WinsTheMatch()
+        {
+            var player1 = new Player("Roger Federer");
+            var player2 = new Player("Andy Murray");
+
+            var gameResultGenerator = GetGeneratorForSetsWon(new[] { true, false, true });
+
+            var match = new Match(player1, player2, gameResultGenerator);
+
+            match.Play();
+
+            match.Winner.Should().Be(player1);
+        }
+
+        [Test]
+        public void WhenTheFirstTwoSetsAreSharedAndPlayer2WinsTheThird_ThenPlayer2WinsTheMatch()
+        {
+            var player1 = new Player("Roger Federer");
+            var player2 = new Player("Andy Murray");
+
+            var gameResultGenerator = GetGeneratorForSetsWon(new[] { true, false, false });
+
+            var match = new Match(player1, player2, gameResultGenerator);
+
+            match.Play();
+
+            match.Winner.Should().Be(player2);
+        }
+
+        [Test]
+        public void WhenPlayer2WinsTheFirstTwoSets_ThenTheyWinTheMatch()
+        {
+            var player1 = new Player("Roger Federer");
+            var player2 = new Player("Andy Murray");
+
+            var gameResultGenerator = GetGeneratorForSetsWon(new[] { false, false });
+
+            var match = new Match(player1, player2, gameResultGenerator);
+
+            match.Play();
+
+            match.Winner.Should().Be(player2);
+        }
+
+        private IGameResultGenerator GetGeneratorForSetsWon(bool[] setWonByPlayer1)
+        {
+            var gameWherePlayer1Won = new Game(ScoreValues.GameWon, ScoreValues.Fifteen);
+            var gameWherePlayer2Won = new Game(ScoreValues.Thirty, ScoreValues.GameWon);
+
+            var gameResultGenerator = new Mock<IGameResultGenerator>(MockBehavior.Strict);
+
+            var games = new List<Game>();
+            foreach(var setWin in setWonByPlayer1)
+            {
+                if (setWin)
+                {
+                    games.AddRange(Enumerable.Repeat(gameWherePlayer1Won, 6));
+                }
+                else
+                {
+                    games.AddRange(Enumerable.Repeat(gameWherePlayer2Won, 6));
+                }
+            }
+
+            var queue = new Queue<Game>(games);
+
+            gameResultGenerator.Setup(call => call.GetResult()).Returns(() => queue.Dequeue());
+
+            return gameResultGenerator.Object;
         }
     }
 }
--- TennisMatch/Models/Match.cs
+++ TennisMatch/Models/Match.cs
@@ -1,20 +1,50 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TennisMatch.Models
 {
     public class Match
     {
+        private readonly IGameResultGenerator gameResultGenerator;
+
         public Player Player1 { get; private set; }
         public Player Player2 { get; private set; }
 
         public IList<Set> Sets { get; set; }
 
-        public Match(Player player1, Player player2) 
+        public Player Winner { get; private set; }
+
+        public Match(Player player1, Player player2, IGameResultGenerator gameResultGenerator) 
         {
             Player1 = player1;
             Player2 = player2;
 
             Sets = new List<Set>();
+
+            this.gameResultGenerator = gameResultGenerator;
+        }
+
+        public void Play()
+        {
+            while(Winner == null)
+            {
+                var set = new Set(Player1, Player2, gameResultGenerator);
+                set.Play();
+
+                Sets.Add(set);
+
+                CheckForWinner();
+            }
+        }
+
+        private void CheckForWinner()
+        {
+            var setsWon = Sets.GroupBy(s => s.Winner);
+            var winner = setsWon.FirstOrDefault(g => g.Count() >= 2);
+            if (winner == null) return;
+
+            Winner = winner.Key;
         }
     }
 }
